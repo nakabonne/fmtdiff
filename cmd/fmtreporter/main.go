632c287b98cf48b33d23ddb98a305dac0e7ac621
nakabonne <nakabonne@gmail.com>
2019-12-28 15:41:49 +0900

    Make it possible to run as a cli tool

---
+++ cmd/fmtreporter/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/nakabonne/fmtreporter"
+)
+
+var (
+	flagSet     = flag.NewFlagSet("fmtreporter", flag.ContinueOnError)
+	localPrefix = flagSet.String("local-prefix", "", "local prefix")
+)
+
+func main() {
+	flagSet.Usage = func() {
+		fmt.Fprintln(os.Stderr, "usage: fmtreporter [flags] [files ...]")
+		flagSet.PrintDefaults()
+	}
+	if err := flagSet.Parse(os.Args[1:]); err != nil {
+		if err != flag.ErrHelp {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		return
+	}
+
+	for _, path := range flagSet.Args() {
+		b, err := fmtreporter.Run(path, *localPrefix)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Println(string(b))
+	}
+}
